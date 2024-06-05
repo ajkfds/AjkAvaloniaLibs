c0827217058b43b5e00cbc8395d42e9b5e631514
ajkfds <ajkfds2018@gmail.com>
2024-06-06 05:23:55 +0900

    add error trap

--- AjkAvaloniaLibs/Contorls/ColorLabelControl.axaml.cs
+++ AjkAvaloniaLibs/Contorls/ColorLabelControl.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Controls.Documents;
 using Avalonia.Media;
 using DynamicData;
 using System.Reflection.Emit;
@@ -14,22 +15,28 @@ namespace AjkAvaloniaLibs.Contorls
 
         public void Clear()
         {
+            if (TextBlock1.Inlines == null) return;
             TextBlock1.Inlines.Clear();
         }
 
         public void Add(ColorLabel colorLabel)
         {
-            foreach(var item in colorLabel.GetItems())
+            if (TextBlock1.Inlines == null) return;
+            foreach (var item in colorLabel.GetItems())
             {
                 if(item is ColorLabel.labelText)
                 {
-                    ColorLabel.labelText label = item as ColorLabel.labelText;
+                    ColorLabel.labelText? label = item as ColorLabel.labelText;
+                    if (label == null) throw new System.Exception();
                     Avalonia.Controls.Documents.Run run = new Avalonia.Controls.Documents.Run(label.Text);
                     if(label.Color != null)
                     {
                         run.Foreground = new SolidColorBrush((Avalonia.Media.Color)label.Color);
                     }
                     TextBlock1.Inlines.Add(run);
+                }else if(item is ColorLabel.labelNewLine)
+                {
+                    TextBlock1.Inlines.Add(new Run("\n"));
                 }
             }
 
